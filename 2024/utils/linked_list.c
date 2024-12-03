fbeb05f90b8c688d17d2359c526deffd534016df
Wisniewski, Dominik <d.wisniewski@prof-schumann.de>
2024-12-03 09:42:00 +0100

    thanks to lowlevel, I was able to know how to structure the struct and I've implmented add and insert.\nThe implementation of pop will be done later

--- 2024/utils/linked_list.c
+++ 2024/utils/linked_list.c
@@ -1,29 +1,114 @@
+// ty low level
 #include <stdio.h>
 #include <stdlib.h>
-struct node {
+
+typedef struct {
+  void *next;
   int value;
-  struct node *next;
-};
+} node;
 
-void push(struct node main, int value) {
-  struct node new;
-  new.next = malloc(sizeof(struct node));
-  new.value = value;
+node *head = NULL;
 
-  *main.next = new;
+node *add(int value) {
+  node *new = NULL;
+  if (head == NULL) {
+    new = malloc(sizeof(node));
+    if (new == NULL) {
+      return NULL;
+    }
+    new->value = value;
+    head = new;
+    new->next = NULL;
+  } else {
+    new = malloc(sizeof(node));
+    if (new == NULL) {
+      return NULL;
+    }
+    new->value = value;
+    new->next = head;
+    head = new;
+  }
+  return new;
+}
+
+/// NOT IMPLEMENTED YET
+void pop(int pos) {
+  printf("Not implemented yet");
+  return;
+
+  node *p_head_next = head;
+  node *p_head_prev = head;
+
+  int cnt = 0;
+  while (cnt < pos && p_head_next->next != NULL) {
+    p_head_next = p_head_next->next;
+    cnt++;
+  }
+  cnt = 0;
+  while (cnt < pos - 1) {
+    p_head_prev = p_head_prev->next;
+    cnt++;
+  }
+}
+
+node *insert(int value, int pos) {
+  node *new = NULL;
+
+  if (pos == 0) {
+    new = malloc(sizeof(NULL));
+    new->value = value;
+    new->next = head;
+    head = new;
+    return new;
+  }
+
+  node *p_head_next = head;
+  node *p_head_prev = head;
+
+  int cnt = 0;
+  while (cnt < pos && p_head_next->next != NULL) {
+    p_head_next = p_head_next->next;
+    cnt++;
+  }
+  cnt = 0;
+  while (cnt < pos - 1) {
+    p_head_prev = p_head_prev->next;
+    cnt++;
+  }
+  if (p_head_next == p_head_prev) {
+    new = malloc(sizeof(node));
+    new->value = value;
+    p_head_next->next = new;
+  } else {
+    new = malloc(sizeof(node));
+    new->value = value;
+    new->next = p_head_next;
+    p_head_prev->next = new;
+  }
+  return new;
+}
+
+void print_list() {
+  node *current = head;
+  while (current != NULL) {
+    printf("%d->", current->value);
+    current = current->next;
+  }
+  printf("NULL\n");
 }
 
 int main() {
-  struct node main;
-  main.value = 0;
-  main.next = malloc(sizeof(struct node));
-
-  push(main, 1);
-  push(*main.next, 2);
-  push(*main.next->next, 3);
-  printf("%d\n", main.value);
-  printf("%d\n", main.next->value);
-  printf("%d\n", main.next->next->value);
-  printf("%d\n", main.next->next->next->value);
+  node *new = add(0);
+  new = add(1);
+  new = add(2);
+  new = add(3);
+  new = add(4);
+  new = add(5);
+  new = add(6);
+  new = add(7);
+  new = add(8);
+  new = add(9);
+  insert(11, 0);
+  print_list();
   return 0;
 }
