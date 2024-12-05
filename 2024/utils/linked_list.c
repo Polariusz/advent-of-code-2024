a7ebe3f90fcaf5ba36dd1c6d068bf49ef9fc6143
Polariusz <truepolariusz@gmail.com>
2024-12-05 22:26:10 +0100

    implemented pop function

--- 2024/utils/linked_list.c
+++ 2024/utils/linked_list.c
@@ -1,4 +1,5 @@
-// ty low level
+/// ty low level for giving me guidance.
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,24 +32,40 @@ node *add(int value) {
   return new;
 }
 
-/// NOT IMPLEMENTED YET
-void pop(int pos) {
-  printf("Not implemented yet");
-  return;
+int pop(int pos) {
+  int value;
+  if (pos == 0) {
+    node *new = malloc(sizeof(node));
+    new = head;
+    value = new->value;
+    new = head->next;
+    head = new;
+    return value;
+  } else {
+    node *p_head_prev = malloc(sizeof(node));
+    if (head != NULL) {
+      p_head_prev = head;
+    }
+    node *p_head_next = malloc(sizeof(node));
+    if (head->next != NULL) {
+      p_head_next = head->next;
+    }
+    if (p_head_next->next == NULL) {
+      head->next = NULL;
+      return value;
+    }
 
-  node *p_head_next = head;
-  node *p_head_prev = head;
+    int cnt = 1;
+    while (cnt < pos) {
+      p_head_prev = p_head_prev->next;
+      p_head_next = p_head_next->next;
+      cnt++;
+    }
+    value = p_head_next->value;
+    p_head_prev->next = p_head_next->next;
+  }
 
-  int cnt = 0;
-  while (cnt < pos && p_head_next->next != NULL) {
-    p_head_next = p_head_next->next;
-    cnt++;
-  }
-  cnt = 0;
-  while (cnt < pos - 1) {
-    p_head_prev = p_head_prev->next;
-    cnt++;
-  }
+  return value;
 }
 
 node *insert(int value, int pos) {
@@ -97,18 +114,49 @@ void print_list() {
   printf("NULL\n");
 }
 
+void println(char *message) { printf("%s\n", message); }
+
 int main() {
+  // Add few numbers
   node *new = add(0);
   new = add(1);
-  new = add(2);
-  new = add(3);
   new = add(4);
-  new = add(5);
   new = add(6);
-  new = add(7);
   new = add(8);
   new = add(9);
-  insert(11, 0);
+  new = add(10);
+  new = add(12);
+  new = add(14);
+  new = add(15);
+  new = add(16);
+  new = add(18);
+  new = add(20);
+  println("After adding:");
+  print_list();
+  println("");
+
+  // Make the list nice by filling the missing numbers
+  new = insert(2, 11);
+  new = insert(3, 11);
+  new = insert(5, 10);
+  new = insert(7, 9);
+  new = insert(11, 6);
+  new = insert(13, 5);
+  new = insert(17, 2);
+  new = insert(19, 1);
+
+  println("After inserting:");
+  print_list();
+  println("");
+  // Pop numbers 0, 11, 17, 19 and 20
+
+  println("Popping:");
+  printf("Index %d: %d\n", 0, pop(20));
+  printf("Index %d: %d\n", 9, pop(9));
+  printf("Index %d: %d\n", 3, pop(3));
+  printf("Index %d: %d\n", 0, pop(0));
+  println("");
+  println("After popping:");
   print_list();
   return 0;
 }
