1a3527634d92c89f258ef8932de19d5c506adbd0
Wisniewski, Dominik <d.wisniewski@prof-schumann.de>
2024-12-13 13:32:57 +0100

    day 3

--- 2024/day3/c/main.c
+++ 2024/day3/c/main.c
@@ -3,6 +3,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+char process(char *token) {
+  char token_arr[strlen(token)];
+  strcpy(token_arr, token);
+
+  if (token_arr[0] < '0' || 0 [token_arr] > '9') {
+    printf(" bad\n");
+    return 0;
+  }
+
+  int result = 0;
+  int mul1 = 0;
+  int mul2 = 0;
+
+  int starting_first_number_index = 0;
+  int ending_first_number_index = -1;
+
+  int starting_second_number_index = -1;
+  int ending_second_number_index = -1;
+
+  for (int i = 0; i < strlen(token); i++) {
+    if (ending_first_number_index == -1) {
+      if (i[token_arr] == ',') {
+        if (token_arr[i + 1] < '0' || token_arr[i + 1 > '9']) {
+          printf(" second after , bad");
+          break;
+        }
+        ending_first_number_index = i - 1;
+        starting_second_number_index = i + 1;
+      } else if (i[token_arr] < '0' || i[token_arr] > '9') {
+        printf(" bad");
+        break;
+      }
+    } else {
+      if (i[token_arr] == ')') {
+        ending_second_number_index = i - 1;
+        break;
+      } else if (i[token_arr] < '0' || i[token_arr] > '9') {
+        printf(" second bad");
+        break;
+      }
+    }
+  }
+  printf("\n");
+  return 0;
+}
+
 int main() {
   FILE *pF = fopen("../input2", "r");
   char buffer[1024];
@@ -12,19 +58,15 @@ int main() {
     return 1;
   }
 
-  regex_t regeex;
-  int value = regcomp(&regeex, "[0-9],[0-9]", 0);
-
   int sum = 0;
   char *token;
   while (fgets(buffer, 1024, pF) != NULL) {
-    token = strtok(buffer, "mul\n");
+    token = strtok(buffer, "mul(\n");
     while (token != NULL) {
-      // printf("Bef regex: %s\n", token);
-      if (regexec(&regeex, token, 0, NULL, 0) == 0) {
-        printf("Aft regex: %s\n", token);
-      }
-      token = strtok(NULL, "mul\n");
+      printf("%s", token);
+      process(token);
+
+      token = strtok(NULL, "mul(\n");
     }
   }
   fclose(pF);
